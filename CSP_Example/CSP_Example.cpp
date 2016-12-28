c46bfd2171a28e786a6099b7e66f5ddb3f37b96f
liyanyan <378912864@qq.com>
2016-12-28 15:54:21 +0800

    bbbbbbbb

--- CSP_Example/CSP_Example.cpp
+++ CSP_Example/CSP_Example.cpp
@@ -18,6 +18,9 @@ BEGIN_MESSAGE_MAP(CCSP_ExampleApp, CWinApp)
 END_MESSAGE_MAP()
 
 
+
+
+
 // CCSP_ExampleApp 构造
 
 CCSP_ExampleApp::CCSP_ExampleApp()
@@ -65,6 +68,9 @@ BOOL CCSP_ExampleApp::InitInstance()
 	// 更改用于存储设置的注册表项
 	// TODO: 应适当修改该字符串，
 	// 例如修改为公司或组织名
+	
+	
+	
 	SetRegistryKey(_T("应用程序向导生成的本地应用程序"));
 
 	CCSP_ExampleDlg dlg;
